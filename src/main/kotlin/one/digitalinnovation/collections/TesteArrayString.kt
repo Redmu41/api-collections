0ed2cc2b751ba10fbdb891a7880d726016a6ca81
Redmu41 <cassio.redmuu@gmail.com>
2021-05-11 18:24:40 -0300

    1 commit - directory

---
+++ src/main/kotlin/one/digitalinnovation/collections/TesteArrayString.kt
@@ -0,0 +1,23 @@
+package one.digitalinnovation.collections
+
+fun main() {
+    val nomes = Array<String>(3) { "" }
+    nomes[0] = "Maria"
+    nomes[1] = "Zezá"
+    nomes[2] = "José"
+
+    for (nome in nomes) {
+        println(nome)
+    }
+
+    println("-------------------------------------")
+
+    nomes.sort()
+    nomes.forEach { println(it) }
+
+    println("-------------------------------------")
+    val nome2 = arrayOf("Maria", "Zezá", "Pedro")
+    nome2.sort()
+    nome2.forEach { println(it) }
+
+}
